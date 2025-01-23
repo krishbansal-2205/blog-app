d4243abd7010a0a5b61bef910a8ad8f4e07b9ed5
Krish Bansal <krishbansalkssk@outlook.com>
2025-01-23 18:53:41 +0530

    setup redux store and add protected pages

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+function Protected({ children, authentication = true }) {
+
+    const authStatus = useSelector(state => state.auth.status);
+    const navigate = useNavigate();
+    const [loader, setLoader] = useState(true);
+
+    useEffect(() => {
+        if (authentication && !authStatus) {
+            navigate("/login");
+        }
+        else if (!authentication && authStatus) {
+            navigate("/");
+        }
+        setLoader(false);
+    }, [authStatus, authentication, navigate])
+
+    return (
+        loader ? <h1>Loading...</h1> : <>{children}</>
+    )
+}
+
+export default Protected
